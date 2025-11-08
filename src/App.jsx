7fe4444cb3acbf0d44e1f5e5b530886ad3e26447
Aleksandar Nachev <nachevalexandar.a@gmail.com>
2025-11-08 10:12:07 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import TodoList from './components/ToDoList.jsx'
+
+function App() {
+
+  return (
+    <div>
+    <h1>Todo List</h1>
+
+    <TodoList/>
+    </div>
+  )
+}
+
+export default App
